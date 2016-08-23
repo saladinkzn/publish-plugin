377bc4d1071bb22bfc0699403bdbff12cb1ef475
sala <saladinkzn@gmail.com>
2016-08-23 23:52:10 +0300

    javadoc bug was fixed

--- src/main/groovy/ru/shadam/publish/PublishPlugin.groovy
+++ src/main/groovy/ru/shadam/publish/PublishPlugin.groovy
@@ -3,9 +3,10 @@ package ru.shadam.publish
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.XmlProvider
+import org.gradle.api.publish.maven.MavenArtifact
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.jvm.tasks.Jar
-
 /**
  * @author sala
  */
@@ -27,7 +28,8 @@ class PublishPlugin implements Plugin<Project> {
 
     if(!project.tasks.findByName('javadocJar')) {
       project.task('javadocJar', type: Jar, description: 'Creates javadoc jar') {
-        dependsOn: project.tasks.javadoc
+        classifier = 'javadoc'
+        dependsOn(project.tasks.javadoc)
       }
     }
 
@@ -39,53 +41,46 @@ class PublishPlugin implements Plugin<Project> {
     project.afterEvaluate { Project it ->
       def errors = validateExtension(extension)
 
-      if(errors) {
+      if (errors) {
         throw new GradleException("Failed to validate extension: ${errors.join(', ')}")
       }
 
-      it.publishing {
-        publications {
-          "${extension.publicationName}"(MavenPublication) {
-            pom {
-              withXml {
-                asNode().with {
-                  appendNode('name', extension.name)
-                  appendNode('description', extension.description)
-                  appendNode('url', extension.projectWebsite)
-                  appendNode('scm').with {
-                    appendNode('url', extension.projectScm)
-                    appendNode('connection', extension.projectScm)
-                    appendNode('developerConnection', extension.projectScm)
-                  }
-                  appendNode('licenses').with {
-                    appendNode('license').with {
-                      appendNode('name', extension.license)
-                      appendNode('url', extension.licenseUrl)
-                      appendNode('distribution', 'repo')
-                    }
-                  }
-                  appendNode('developers').with {
-                    appendNode('developer').with {
-                      appendNode('id', extension.developerId)
-                      appendNode('name', extension.developerName)
-                    }
-                  }
-                  it
-                }
-              }
+      def pc = it.publishing.publications
+      MavenPublication p = pc.create(extension.publicationName, MavenPublication)
+      p.pom.withXml { XmlProvider xp ->
+        xp.asNode().with {
+          appendNode('name', extension.name)
+          appendNode('description', extension.description)
+          appendNode('url', extension.projectWebsite)
+          appendNode('scm').with {
+            appendNode('url', extension.projectScm)
+            appendNode('connection', extension.projectScm)
+            appendNode('developerConnection', extension.projectScm)
+          }
+          appendNode('licenses').with {
+            appendNode('license').with {
+              appendNode('name', extension.license)
+              appendNode('url', extension.licenseUrl)
+              appendNode('distribution', 'repo')
             }
-
-            from project.components.java
-//
-            artifact(project.tasks.sourcesJar) {
-              classifier 'sources'
-            }
-
-            artifact (project.tasks.javadocJar) {
-              classifier "javadoc"
+          }
+          appendNode('developers').with {
+            appendNode('developer').with {
+              appendNode('id', extension.developerId)
+              appendNode('name', extension.developerName)
             }
           }
         }
+      }
+
+      p.from(it.components.getByName("java"))
+//
+      p.artifact(it.tasks.sourcesJar) { MavenArtifact ma ->
+        ma.classifier = 'sources'
+      }
+
+      p.artifact(it.tasks.javadocJar) { MavenArtifact ma ->
+        ma.classifier = 'javadoc'
       }
     }
   }
